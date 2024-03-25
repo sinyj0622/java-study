b344434b9d77defd842a3fd95d9e1eb7be1ea777
sinyj0622 <euuun22@naver.com>
2024-03-25 17:17:04 +0900

    회원관리 예제

---
+++ hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,23 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void join() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void findMember() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
